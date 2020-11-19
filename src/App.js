587efb938c6a832c74c5f24714e39602334d6d5d
cdondi <clivedondi@hotmail.com>
2020-11-19 03:30:58 -0600

    Added styling to page elements.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import './homepage.styles.scss';
+import './pages/homepage/homepage.styles.scss';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
