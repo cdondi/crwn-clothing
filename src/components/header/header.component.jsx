d152fb4d808fbe20dbb00ab7dae62511b1081f26
cdondi <clivedondi@hotmail.com>
2020-12-06 02:19:55 -0600

    Added checkout page and checkout component.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -26,7 +29,7 @@ const Header = ({ currentUser, hidden }) => (
                 currentUser ?
                     (
                         <div className='option' onClick={() => auth.signOut()}>
-                            SIGN OUT
+                            SIGN OUT 
                         </div>
                     )
                     :
@@ -46,9 +49,14 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 );
 
-const mapStateToProps = ({user: {currentUser }, cart: {hidden}}) => ({
+/*const mapStateToProps = ({user: {currentUser }, cart: {hidden}}) => ({
     currentUser,
     hidden
+});*/
+
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
