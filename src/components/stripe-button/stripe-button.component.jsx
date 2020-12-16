dd9d46196e2e2bdef466f4ed3db6ffbb39951d74
cdondi <clivedondi@hotmail.com>
2020-12-15 20:39:20 -0600

    Added stripe payments. Added favicon and title.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HyoxmG6r7PN6yDthxjz75Vd6HkqNOn1tGlVkMxgeCJ7QmnuqwHkHxRb2Bx6kV3fihuf1tuPZBcsQu5qCyCZp4QW00Vz29wUd3';
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
