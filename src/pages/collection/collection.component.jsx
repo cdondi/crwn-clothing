9b56c106aed3b92caeee3d38501cd5023133ffd0
cdondi <clivedondi@hotmail.com>
2020-12-15 19:30:58 -0600

    Moved directory and shop data into its own reducer. Created respective selectors.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 //import { createStructuredSelector } from 'reselect';
 
-//import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ( { match }) => {
-    console.log(match.params.collectionId);
+const CollectionPage = ( { collection }) => {
+    const { title, items } = collection;
     return (
-        <div className='collection'>
-            <h2>COLLECTION PAGE</h2>
+        <div className='collection-page'>
+            <h2 className='title'>{ title }</h2>
+            <div className='items'>
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
+            </div>
         </div>
     );
 };
